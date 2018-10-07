020edd5023257fa8a1744d2823df1b1b5ea85867
Eric Grube <grubee79@gmail.com>
2018-10-07 12:03:14 -0400

    small refactor and cleanup

--- popup.js
+++ popup.js
@@ -7,13 +7,9 @@ chrome.tabs.executeScript({
     code: "window.getSelection().toString();"
 }, async (selection) => {
     const translate = selection[0];
-    const detectedLang = await detectLanguage(selection);
-    const lang = JSON.parse(detectedLang).data.detections[0][0].language;
-    const transDetails = {
-        selectionToTranslate: translate,
-        detectedLang: lang
-    }
-    const translations = await getTranslations(transDetails);
+    const lang = await detectLanguage(selection);
+    const detectedLang = JSON.parse(lang).data.detections[0][0].language;
+    const translations = await getTranslations(translate, detectedLang);
     const english = processTranslationObj(translations.english);
     const german = processTranslationObj(translations.german);
     const spanish = processTranslationObj(translations.spanish);
@@ -22,47 +18,44 @@ chrome.tabs.executeScript({
     document.getElementById("spanishText").innerHTML = `Español: ${spanish}`;
 });
 
-async function getTranslations(transDetails) {
-    const { selectionToTranslate, detectedLang } = transDetails;
+async function getTranslations(translate, lang) {
     let totalTranslations = {
         english: undefined,
         spanish: undefined,
         german: undefined
     };
-    if (detectedLang === ENGLISH) {
-        const german = getTranslation(selectionToTranslate, GERMAN);
-        const spanish = getTranslation(selectionToTranslate, SPANISH);
+    if (lang === ENGLISH) {
+        const german = getTranslation(translate, GERMAN);
+        const spanish = getTranslation(translate, SPANISH);
         const translations = await Promise.all([german, spanish]);
-        totalTranslations.english = selectionToTranslate;
+        totalTranslations.english = translate;
         totalTranslations.german = translations[0];
         totalTranslations.spanish = translations[1];
-        return totalTranslations;
     }
-    else if (detectedLang === SPANISH) {
-        const german = getTranslation(selectionToTranslate, GERMAN);
-        const english = getTranslation(selectionToTranslate, ENGLISH);
+    else if (lang === SPANISH) {
+        const german = getTranslation(translate, GERMAN);
+        const english = getTranslation(translate, ENGLISH);
         const translations = await Promise.all([german, english]);
-        totalTranslations.spanish = selectionToTranslate;
+        totalTranslations.spanish = translate;
         totalTranslations.german = translations[0];
         totalTranslations.english = translations[1];
-        return totalTranslations;
     }
     else {
-        const spanish = getTranslation(selectionToTranslate, SPANISH);
-        const english = getTranslation(selectionToTranslate, ENGLISH);
+        const spanish = getTranslation(translate, SPANISH);
+        const english = getTranslation(translate, ENGLISH);
         const translations = await Promise.all([spanish, english]);
         totalTranslations.spanish = translations[0];
         totalTranslations.english = translations[1];
-        totalTranslations.german = selectionToTranslate;
-        return totalTranslations;
+        totalTranslations.german = translate;
     }
+    return totalTranslations;
 }
 
-function getTranslation(selectionToTranslate, lang) {
+function getTranslation(translate, lang) {
     return new Promise((res, rej) => {
-        const TRANSLATE_REQUEST = `https://translation.googleapis.com/language/translate/v2?q=${selectionToTranslate}&target=${lang}&key=${API_KEY}`;
-        let xmlHttpReq = new XMLHttpRequest();
-        xmlHttpReq.onreadystatechange = function () {
+        const TRANSLATE_REQUEST = `https://translation.googleapis.com/language/translate/v2?q=${translate}&target=${lang}&key=${API_KEY}`;
+        const xmlHttpReq = new XMLHttpRequest();
+        xmlHttpReq.onreadystatechange = () => {
             if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
                 res(xmlHttpReq.responseText);
             }
@@ -75,28 +68,24 @@ function getTranslation(selectionToTranslate, lang) {
 function detectLanguage(selectionToDetect) {
     return new Promise((res, rej) => {
         const DETECT_REQUEST = `https://translation.googleapis.com/language/translate/v2/detect?key=${API_KEY}`;
-        let xmlHttpReq = new XMLHttpRequest();
+        const xmlHttpReq = new XMLHttpRequest();
         xmlHttpReq.open("POST", DETECT_REQUEST, true);
         xmlHttpReq.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
-        xmlHttpReq.onreadystatechange = function () {
+        xmlHttpReq.onreadystatechange = () => {
             if (xmlHttpReq.readyState == 4 && xmlHttpReq.status == 200) {
                 res(xmlHttpReq.responseText);
             }
         }
-
         xmlHttpReq.send(`q=${selectionToDetect}`);
-
     });
 }
 
 function processTranslationObj(transObj) {
     try {
-        let json = JSON.parse(transObj);
-        console.log(json);
+        const json = JSON.parse(transObj);
         return json.data.translations[0].translatedText;
     } catch (err) {
-        console.log(transObj);
         return transObj;
     }
 }
